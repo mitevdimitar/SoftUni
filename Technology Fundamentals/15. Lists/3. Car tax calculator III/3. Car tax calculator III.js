bde459260f40db11a2ff87964b434c8c6a85b8c7
mitevdimitar <37977187+mitevdimitar@users.noreply.github.com>
2018-11-18 09:49:27 +0200

    Create Car Tax Calculator III

---
+++ Technology Fundamentals/15. Lists/3. Car tax calculator III/3. Car tax calculator III.js
@@ -0,0 +1,55 @@
+	let totalTax = kW * age;
+	let result = `${totalTax.toFixed(2)} lv.`;
+	return result;
+}
+
+function totalCalculationMotor(kW) {
+	let tax = 0;
+	if (kW > 750) {
+		tax = 125;
+	} else if (kW > 490) {
+		tax = 94;
+	} else if (kW > 350) {
+		tax = 63;
+	} else if (kW > 250) {
+		tax = 44;
+	} else if (kW > 125) {
+		tax = 31;
+	} else {
+		tax = 15;
+    }
+    tax = tax.toFixed(2);	
+    let result1 = `${tax} lv.`;
+	return result1;
+}
+
+function calculate(kW) {
+	let tax = 0;
+	if (kW > 110) {
+		tax = +kW * 1.54;
+	} else if (kW > 74) {
+		tax = +kW * 1.38;
+	} else if (kW > 55) {
+		tax = +kW * 0.68;
+	} else if (kW > 37) {
+		tax = kW * 0.50;
+	} else {
+		tax = kW * 0.43;
+    }
+    tax = tax.toFixed(2);
+    return tax;
+}
+
+function taxDependingOnAge(age) {
+	let index = 1;
+	if (age < 5) {
+		index *= 2.80;
+	} else if (age < 14) {
+		index *= 1.50;
+	} else {
+		index *= 1.00;
+	}
+	return index;
+}
+
+}
