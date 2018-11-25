1b4c90b59d4870049109ee1b4f0d8ea296f31946
mitevdimitar <37977187+mitevdimitar@users.noreply.github.com>
2018-11-25 20:06:16 +0200

    Create 03. Town Info.js

---
+++ Technology Fundamentals/7. Data Types and Variables - Exercise/03. Town Info/03. Town Info.js
@@ -0,0 +1,5 @@
+function solve(townName, population, area) {
+	population = Number(population);
+	area = Number(area);
+	console.log(`Town ${townName} has population of ${population} and area ${area} square km.`);
+}
