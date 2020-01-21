86612a01ff0b086e3ea324e72703d127b737ace0
mitevdimitar <37977187+mitevdimitar@users.noreply.github.com>
2020-01-21 22:25:10 +0200

    Create stringLength.js

---
+++ JS Core/JS Fundamentals/3. Lab: Syntax, Functions and Statements/01. String Length/stringLength.js
@@ -0,0 +1,6 @@
+function solve(str1, str2, str3) {
+	let totalLength = str1.length + str2.length + str3.length;
+	let averageLength = totalLength / 3;
+	console.log(totalLength);
+	console.log(Math.floor(averageLength));
+}
