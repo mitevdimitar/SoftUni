a43964f26039d6c38f24a5ddb822f73c52bbc1c4
Dimitar <37977187+mitevdimitar@users.noreply.github.com>
2020-03-25 13:53:23 +0200

    Create functionalSum.js

---
+++ JS Core/JS Advanced/02. Exercise: Advanced Functions/06. Functional Sum/functionalSum.js
@@ -0,0 +1,14 @@
+(function () {
+    let sum = 0;
+    function add(num) {
+       sum += num;
+      return add;
+    }
+
+    add.toString = function() {
+        return sum;
+    };
+
+    return add;
+
+})();
